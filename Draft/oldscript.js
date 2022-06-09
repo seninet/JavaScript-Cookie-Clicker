265f8bb9d270dd8bc4fec3e421dbb477a17ab0ed
AIchrak <ichrak.aissia@outlook.com>
2022-06-09 09:34:47 +0200

    final design update

--- Draft/oldscript.js
+++ Draft/oldscript.js
@@ -2,29 +2,31 @@
     // Describe variables:
         let imageBtn = document.getElementById("imagebtn");
         let counterInput = document.getElementById("counter");
-        let score=document.getElementById("scorebox");
-        let conteur=document.getElementById("conteur");
         let counter = 0;
-        var bonus = document.getElementById("btn-2");
+        var autoClick = 0;
+       // var bonus = document.getElementById("btn-2");
+        var timer=document.getElementById("timer");
+       // var bonustime =5;
+       // var bonusCost=10;
+        var bonus200=1;
         //var bonus200=1;
     
         // Update function updates the HTML text in buttons, you need to call this function in other functions (buttons...):
         function update(){
             //counter = counter + multiplier*bonus200;
             counterInput.value = counter; 
-            document.getElementById("costautoclick").innerHTML  = "Price= " + ((autoClick +1)*20) +  "pts";
+            document.getElementById("costautoclick").innerHTML  =  ((autoClick +1)*20) +  " pts";
             
             document.getElementById("persecondcookies").innerHTML  = (autoClick + 1) + " point(s)/second";
             document.getElementById("amountmultiplier").innerHTML  = " your clicks" + " x" + (multiplier+2);
-            document.getElementById("costmultiplier").innerHTML  = "Price= " + ((multiplier+2)*10)  + " pts";
-            //document.getElementById("bonuscost").innerHTML  = "Price= " + bonusCost  + " pts";
-           // document.getElementById("timer").innerHTML  = bonusTime + " seconds";
+            document.getElementById("costmultiplier").innerHTML  =  ((multiplier+2)*10)  + " pts";
+            //document.getElementById("bonuscost").innerHTML  = (bonus200*10)  + " pts";
         }
 
         // Creates the click function on the cookie image:
         imageBtn.onclick=function(){clickfunction();};
         function clickfunction(){
-            counter = counter + 1;  
+            counter = counter + 1; 
             update();
             toaddmultipliervaluetoscore();  
             //addscorebonus();
@@ -41,12 +43,12 @@
         }
             
         //Auto-clicker:
-        var autoClick = 0;
+        
             
         function timer() {
             counter = counter + autoClick; 
-            
-        update();}
+            update();
+        }
         setInterval(timer, 1000);        
         let autoclicker= document.getElementById("btn-3");
         autoclicker.onclick= function() {increase();}
@@ -84,7 +86,7 @@
         }
         //To make the multiplier work separately you create this function which add the value of multiplie to each click. 
         function toaddmultipliervaluetoscore() {
-            counter = counter + (multiplier-1); 
+            counter = counter + (multiplier-1)*bonus200; 
             update();
             }
         
@@ -128,93 +130,51 @@
             }
         }
 
-        function btn2colorchange(){      
-                
-            if(((multiplier+2)*10)<=30){
-                document.getElementById("btn-2").style.backgroundColor= "#804000";
-            }
-            else if(30<((multiplier+2)*10) && ((multiplier+2)*10) <=50){
-                document.getElementById("btn-2").style.backgroundColor= "#A06300";
-            }
-            else if(50<((multiplier+2)*10) && ((multiplier+2)*10)<=70){
-                document.getElementById("btn-2").style.backgroundColor= "#C08600";
-            }
-            else if(70<((multiplier+2)*10) && ((multiplier+2)*10)<=90){
-                document.getElementById("btn-2").style.backgroundColor= "#E0A900";
-            }
-            else{
-                document.getElementById("btn-2").style.backgroundColor= "#FFCC00";
-            }
-        }
+      
+        //Bonus timer:
 
-        //click sound when we click on the buttons
+        /*bonus.onclick= function() {functionbonus();};
+        
+        function functionbonus(){
+            
+            if(counter>=(bonus200*10)){
+                counter = counter - (bonus200*10);
+                toaddmultipliervaluetoscore()*2;
+                document.getElementById("btn-2").style.backgroundColor= "#FFCC00";
+                var downloadTimer = setInterval(function(){
+                    bonustime--;
+                document.getElementById("timer").textContent = "00:"+bonustime;
+                if(bonustime <= 0)
+                    clearInterval(downloadTimer);
+                    document.getElementById("btn-2").style.backgroundColor= "#804000";
+                },1000);
+            }
+            else {
+                alert("You don't have enough points to buy this improvement!");
+                
+            }        
+            update();
+        };
+       /* function addscorebonus(){
+            counter =counter +(multiplier*bonus200);
+            
+        };*/
+        
+})();
+
+/*//click sound when we click on the buttons
         const audio = new Audio();
         audio.src = "click.mp3";
         // audio
         function playMyAudio(){
-            document.getElementById("myAudio").play();
-        document.getElementById("audioStatus").innerHTML="Audio is Playing";	 
+           document.getElementById("myAudio").play();
+           document.getElementById("audioStatus").innerHTML="Audio is Playing"; 	 
         
         }
         function pauseMyAudio(){
             document.getElementById("myAudio").pause();
-        document.getElementById("audioStatus").innerHTML="Audio Paused";	
-        }
-      
-        //Bonus timer:
-
-        var bonusCost = 10;
-        var bonusOn = false;
-        var bonusTime=30;
-        var scoree=0;
-        function displayBonus() {  
-            document.getElementById("bonuscost").innerHTML  = "Price= " + bonusCost  + " pts";
-        }
-        function displayBonusTime() {  
-            document.getElementById("timer").innerHTML  = bonusTime + " seconds"; 
-        }
-
-        function bonusEnabler() {  
-            if (!bonusOn && scoree >= bonusCost) {    
-                bonus.disabled = false;  
-            } 
-            else {  bonus.disabled = true;  
-            }
-        }
-        function buttonsEnabler() {
-            bonusEnabler();
-          }
-        function enableBonus() {  
-            counter -= bonusCost;  
-            bonusOn = true;  
-            //counter *= 2;  
-            bonus.disabled = true;   
-            displayBonusTime();
-        }
-        function disableBonus() {
-            bonusOn = false;
-            bonusTime = 30;
-            //imageBtn = multiplier;
-            displayBonus();
-            buttonsEnabler();
-          }
-        function bonusF() {  
-            
-            bonus200=2;
-            if (bonusOn) {    
-                --bonusTime;    
-                displayBonusTime();    
-                if (bonusTime === 0) {      
-                    disableBonus();    
-                }  
-            }
-        }
-        displayBonus();
-        //bonus.disabled = true;
-        bonus.addEventListener('click', enableBonus);
-        bonusInterval = window.setInterval(bonusF, 1000);
-
-})();
+            document.getElementById("audioStatus").innerHTML="Audio is Paused";	
+        }*/
 
 /*
 //Add Hover effect on cookie when clicked:
@@ -239,6 +199,47 @@
             update();
         }
         function addscorebonus(){
-            counter = counter*2;
+            counter =counter +multiplier*2;
             
         }*/
+
+/*// Timer working:
+        function functionbonus(){
+                var downloadTimer = setInterval(function(){
+                    bonustime--;
+                document.getElementById("timer").textContent = "00:"+bonustime;
+                if(bonustime <= 0)
+                    clearInterval(downloadTimer);
+                },1000);
+        }
+*/
+
+/* // bonus
+
+        bonus.onclick= function() {functionbonus();};
+        var timer=document.getElementById("timer");
+        var bonustime =30;
+        var bonusCost=10;
+        function functionbonus(){
+            if(counter>=bonusCost){
+                counter = counter - bonusCost;
+                document.getElementById("btn-2").style.backgroundColor= "#FFCC00";
+                var downloadTimer = setInterval(function(){
+                    bonustime--;
+                document.getElementById("timer").textContent = "00:"+bonustime;
+                if(bonustime <= 0)
+                    clearInterval(downloadTimer);
+                    document.getElementById("btn-2").style.backgroundColor= "#804000";
+                },1000);
+            }
+            else {
+                alert("You don't have enough points to buy this improvement!");
+                
+            }        
+            update();
+        };
+        function addscorebonus(){
+            counter = counter*2;
+            
+        };
+*/
